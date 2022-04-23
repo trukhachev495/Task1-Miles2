72c08069ee7066a2ab1bc12a0abe1f576a4862ec
trukhachev495 <sergey.trainings@gmail.com>
2022-04-23 20:40:46 +0300

    добавил файлы

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 500;
+        int amountOfRublesForOneMile = 20;
+        int bonusMiles = ticketPrice / amountOfRublesForOneMile;
+        System.out.println("Начисленно бонусных милль: " + bonusMiles);
+
+
+    }
+
+
+}
